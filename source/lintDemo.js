42ea5a2c03696988686caec4b365d8d918200d32
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-05-16 23:35:52 -0700

    clean up the lint demo to pass linting

--- source/lintDemo.js
+++ source/lintDemo.js
@@ -2,6 +2,9 @@ let a = 1;
 
 let doubleQuotes = 'asdf'; // this text was originally with double quotes
 
-function sample(arg1, arg2) {}
+function sample(arg1, arg2) {
+    a = arg1;
+    doubleQuotes = arg2;
+}
 
 sample(a, doubleQuotes);
