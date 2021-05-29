ae9ca7b4b6fa8741218a7855cf2f3c25c860a60b
Artyom M <artyom.muradyan2001@gmail.com>
2021-05-29 12:47:34 -0700

    Fix linting issues with clock.js

--- source/clock.js
+++ source/clock.js
@@ -1,22 +1,23 @@
 // Popup for the clock button
 document.addEventListener('DOMContentLoaded', () => {
-    let clockWindow = document.getElementById('popup-clock');
-    let clockImg = document.getElementById('clock-img');
-    let submitButton = document.getElementById('submit-button');
-    let startTime = document.getElementById('start-time');
-    let endTime = document.getElementById('end-time');;
+    const clockImg = document.getElementById('clock-img');
+    const submitButton = document.getElementById('clock-submit-button');
+    //const clockWindow = document.getElementById('popup-clock');
+    //const startTime = document.getElementById('start-time');
+    //const endTime = document.getElementById('end-time');
 
-    function toggle(element) {
-        if(element.style.display == 'block') {
+    function toggle(elementID, displayType) {
+        let element = document.getElementById(elementID); 
+        if(element.style.display === displayType) {
             element.style.display = 'none';
         } else {
-            element.style.display = "block";
+            element.style.display = displayType;
         }
     }
 
-    clockImg.addEventListener('click', () => { toggle(clockWindow); });
+    clockImg.addEventListener('click', () => { toggle('popup-clock', 'flex'); });
     
-    submitButton.addEventListener('click', () => { toggle(clockWindow); });
+    submitButton.addEventListener('click', () => { toggle('popup-clock', 'flex'); });
 });
 
 
