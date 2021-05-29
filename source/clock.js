00ede410b6f42056c770215565d5ab03e3f6de55
Artyom M <artyom.muradyan2001@gmail.com>
2021-05-29 12:56:21 -0700

    final fixes for linting

--- source/clock.js
+++ source/clock.js
@@ -2,12 +2,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const clockImg = document.getElementById('clock-img');
     const submitButton = document.getElementById('clock-submit-button');
-    //const clockWindow = document.getElementById('popup-clock');
-    //const startTime = document.getElementById('start-time');
-    //const endTime = document.getElementById('end-time');
+    // const clockWindow = document.getElementById('popup-clock');
+    // const startTime = document.getElementById('start-time');
+    // const endTime = document.getElementById('end-time');
 
+    /**
+     * Toggle the display of the HTML element with elementID between displayType and none   
+     * @param {string} elementID - The ID for the HTML element
+     * @param {string} displayType - The desired dispaly type
+     */
     function toggle(elementID, displayType) {
-        let element = document.getElementById(elementID); 
+        const element = document.getElementById(elementID); 
         if(element.style.display === displayType) {
             element.style.display = 'none';
         } else {
