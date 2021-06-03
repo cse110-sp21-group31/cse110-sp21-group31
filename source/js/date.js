38b19bbc63d9dc3b723962abb792681b56b4c4ad
JacksAwOLF <55404552+JacksAwOLF@users.noreply.github.com>
2021-06-03 12:51:22 -0700

    Update source/js/date.js
    Co-authored-by: Bryce Tsuyuki <btsuyuki1@gmail.com>

--- source/js/date.js
+++ source/js/date.js
@@ -11,7 +11,7 @@ function getDaysKey(dateObj) {
     return [
         now.getFullYear(),
         ('0' + (now.getMonth() + 1)).slice(-2),
-        ('0' + now.getDate()).slice(-2),
+        (`0${  now.getDate()}`).slice(-2),
     ].join('-');
 }
 
