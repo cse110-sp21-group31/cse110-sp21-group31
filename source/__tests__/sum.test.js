e4753ae09a34dec9a7ce70ce89a223e5f4cb2b00
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-06-03 22:06:19 -0700

    fix the sum tests to use import for modules

--- source/__tests__/sum.test.js
+++ source/__tests__/sum.test.js
@@ -1,5 +1,6 @@
-const sum = require('../build/sum.js');
+import sum from '../build/sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
+    // expect(1 + 2).toBe(3);
     expect(sum(1, 2)).toBe(3);
 });
