925ce89a1805b58347cefc0e4cd340f6b4c9a315
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-06-03 22:30:46 -0700

    additional date tests

--- source/__tests__/date.test.js
+++ source/__tests__/date.test.js
@@ -5,7 +5,17 @@ test('getName returns Thursday, June 3 for the date 6/3/2021', () => {
     expect(getName(date)).toBe('Thursday, June 3');
 });
 
-test('getDaysKey gives the day', () => {
+test('getDaysKey gives the string 2021-05-17 for that Date', () => {
     const dateObj = new Date('2021-05-17T00:00:00');
     expect(getDaysKey(dateObj)).toBe('2021-05-17');
+});
+
+test('getDaysKey gives the string 2021-12-17 for that Date', () => {
+    const dateObj = new Date('2021-12-17T00:00:00');
+    expect(getDaysKey(dateObj)).toBe('2021-12-17');
+});
+
+test('getDaysKey gives the string 2021-12-05 for that Date', () => {
+    const dateObj = new Date('2021-12-05T00:00:00');
+    expect(getDaysKey(dateObj)).toBe('2021-12-05');
 });
