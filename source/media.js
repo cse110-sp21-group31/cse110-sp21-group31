d11429ca103248d0b4777c8841f6f8ee4647286a
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-06-07 20:24:31 -0700

    remove console log statement

--- source/media.js
+++ source/media.js
@@ -6,27 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const youtubeTest = document.getElementById('youtube-test');
 
     /**
-     * Toggle the display of the HTML element with elementID between displayType and none   
+     * Toggle the display of the HTML element with elementID between displayType and none
      * @param {string} elementID - The ID for the HTML element
      * @param {string} displayType - The desired dispaly type
      */
     function toggle(elementID, displayType) {
-        const element = document.getElementById(elementID); 
-        if(element.style.display === displayType) {
+        const element = document.getElementById(elementID);
+        if (element.style.display === displayType) {
             element.style.display = 'none';
         } else {
             element.style.display = displayType;
         }
     }
 
-    mediaImg.addEventListener('click', () => { toggle('popup-media', 'flex'); });
-    
-    submitButton.addEventListener('click', () => { 
+    mediaImg.addEventListener('click', () => {
+        toggle('popup-media', 'flex');
+    });
+
+    submitButton.addEventListener('click', () => {
         toggle('popup-media', 'flex');
         if (url.value.includes('youtube')) {
-            console.log(`https://www.youtube.com/embed/${  url.value.substring(32)}`);
-            youtubeTest.src = `https://www.youtube.com/embed/${  url.value.substring(32)}`;
+            // console.log(`https://www.youtube.com/embed/${  url.value.substring(32)}`);
+            youtubeTest.src = `https://www.youtube.com/embed/${url.value.substring(
+                32
+            )}`;
         }
-        document.getElementById('media-uploader').reset(); 
+        document.getElementById('media-uploader').reset();
     });
 });
