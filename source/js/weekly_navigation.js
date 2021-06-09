db8d0d0ea932b9262e6ab6046ba09150a52d7095
Harshil Panchal <harshilpanchal00@gmail.com>
2021-06-09 15:08:01 -0700

    started work on js nav file

--- source/js/weekly_navigation.js
+++ source/js/weekly_navigation.js
@@ -48,7 +48,7 @@ let sideBarClick = function sideBarClickedTemp() {};
  * @param {string} key - The date of the journal
  */
 function populate(key) {
-    const log = getDaysData(key);
+    // removeAll();
     const name = getName(key);
 
     const title = [
@@ -60,10 +60,116 @@ function populate(key) {
 
     document.getElementsByTagName('h3')[0].innerText = title;
 
+    // Test Events
+    let test_events = {
+        '2021-06-07': {
+            name: "Thursday, May 13th",
+            notepad: "blan blah blah",
+            tasks: [
+                {
+                    content: "Go on a run",
+                    completed: true/false,
+                    tags: ["Other"]
+                },
+            ],
+            events: [
+                {
+                    content: "CSE 110 Lecture",
+                    tags: "Lecture",
+                    from: "4:30 AM",
+                    to: "7:30 AM"
+                }, 
+            ],
+            media: [
+                "link1", "link2",
+            ]
+        },
+
+        '2021-06-07': {
+            name: "Thursday, May 13th",
+            notepad: "blan blah blah",
+            tasks: [
+                {
+                    content: "Go on a run",
+                    completed: true/false,
+                    tags: ["Other"]
+                },
+            ],
+            events: [
+                {
+                    content: "CSE 110 Lecture",
+                    tags: "Lecture",
+                    from: "2:15 PM",
+                    to: "5:37 PM"
+                }, 
+            ],
+            media: [
+                "link1", "link2"
+            ]
+        },
+
+        '2021-06-09': {
+            name: "Thursday, May 13th",
+            notepad: "blan blah blah",
+            tasks: [
+                {
+                    content: "Go on a run",
+                    completed: true/false,
+                    tags: ["Other"]
+                },
+            ],
+            events: [
+                {
+                    content: "CSE 110 Lecture",
+                    tags: "Lecture",
+                    from: "2:05 AM",
+                    to: "3:20 AM"
+                }, 
+            ],
+            media: [
+                "link1", "link2"
+            ]
+        },
+
+        '2021-06-11': {
+            name: "Thursday, May 13th",
+            notepad: "blan blah blah",
+            tasks: [
+                {
+                    content: "Go on a run",
+                    completed: true/false,
+                    tags: ["Other"]
+                },
+            ],
+            events: [
+                {
+                    content: "CSE 110 Lecture",
+                    tags: "Lecture",
+                    from: "4:00 AM",
+                    to: "5:30 AM"
+                }, 
+            ],
+            media: [
+                "link1", "link2"
+            ]
+        },
+    }
+
     // Sets all dates for each day
     const days = document.getElementsByClassName('date');
     for (let i = 0; i < 7; i += 1) {
+
+        // loop through every log
         const tempKey = getDaysKey(window.curDate);
+        const log = getDaysData(tempKey);
+        const allTasks = log.tasks;
+        // const allEve = log.events;
+        
+        // CHANGE BELOW TO ONLY HAVE ONE DAY INSTEAD OF ALL
+        const allEve = test_events
+        
+        console.log(allEve);
+
         // eslint-disable-next-line prefer-destructuring
         days[i].innerText = getName(tempKey).split(',')[1];
         window.curDate.setDate(window.curDate.getDate() + 1);
@@ -71,10 +177,7 @@ function populate(key) {
 
     // Reset curDate to beginning of week
     window.curDate.setDate(window.curDate.getDate() - 7);
-    // removeAll();
 
-    const allTasks = log.tasks;
-    const allEve = log.events;
 
     allTasks.forEach((task) => {
         const newTask = document.createElement('task-log');
@@ -84,8 +187,8 @@ function populate(key) {
 
     allEve.forEach((event) => {
         const newEvent = document.createElement('event-log');
-        newEvent.content = event;
-        eventArea.appendChild(newEvent);
+        newEvent.content = event;      
+        // eventArea.appendChild(newEvent);
     });
 
     // update the sidebar
