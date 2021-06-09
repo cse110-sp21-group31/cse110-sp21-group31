226b52d81bcb419700fa183571cfe9294c80d0e5
Harshil Panchal <harshilpanchal00@gmail.com>
2021-06-08 19:30:53 -0700

    fixed overflow property and margins for media-item

---
+++ source/media.js
@@ -0,0 +1,44 @@
+// Popup for the media button
+document.addEventListener('DOMContentLoaded', () => {
+    const mediaImg = document.getElementById('media-img');
+    const submitButton = document.getElementById('media-submit-button');
+    const url = document.getElementById('media-url');
+    const youtubeTest = document.getElementById('youtube-test');
+    const pinterestTest = document.getElementById('pinterest-test');
+    const soundcloudTest = document.getElementById('soundcloud-test');
+
+    /**
+     * Toggle the display of the HTML element with elementID between displayType and none
+     * @param {string} elementID - The ID for the HTML element
+     * @param {string} displayType - The desired dispaly type
+     */
+    function toggle(elementID, displayType) {
+        const element = document.getElementById(elementID);
+        if (element.style.display === displayType) {
+            element.style.display = 'none';
+        } else {
+            element.style.display = displayType;
+        }
+    }
+
+    mediaImg.addEventListener('click', () => {
+        toggle('popup-media', 'flex');
+    });
+
+    submitButton.addEventListener('click', () => {
+        toggle('popup-media', 'flex');
+        if (url.value.includes('youtube')) {
+            // console.log(`https://www.youtube.com/embed/${  url.value.substring(32)}`);
+            youtubeTest.src = `https://www.youtube.com/embed/${url.value.substring(
+                32
+            )}`;
+        }
+        if (url.value.includes('pinterest')) {
+            pinterestTest.src = `https://assets.pinterest.com/ext/embed.html?id=${  url.value.substring(30, 46)}`;
+        }
+        if (url.value.includes('soundcloud')) {
+            soundcloudTest.src = `https://w.soundcloud.com/player/?url=${  url.value}`;
+        }    
+        document.getElementById('media-uploader').reset();
+    });
+});
