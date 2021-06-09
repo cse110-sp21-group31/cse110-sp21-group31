9908e4be83cf3c98c0093f1b7b096d92d9f164a8
Bryce Tsuyuki <btsuyuki1@gmail.com>
2021-06-09 16:09:32 -0700

    additional tests for storage.js

--- source/__tests__/storage.test.js
+++ source/__tests__/storage.test.js
@@ -4,6 +4,8 @@ import {
     addTask,
     addEvent,
     updateTaskChecked,
+    addCustomTagStorage,
+    getCustomTags,
 } from '../js/storage.js';
 
 // define variables that will be used throughout this test suite
@@ -31,13 +33,9 @@ const task2Json = {
     tags: ['UCSD'],
 };
 
-test('getCustomTagColor for tag that does not exist should return empty string', () => {
+test('getCustomTagColor for tag that does not exist should return empty string - before any tags are added', () => {
     expect(getCustomTagColor('nonexistentTag')).toBe('');
 });
-
-// TODO getCustomTagColor tests after some custom tags are added
-
-// TODO tests to check for existence of custom tags after they are added
 
 test('addEvent is successful creating the first event of the day', () => {
     expect(addEvent(daysKey, event1Json)).toBe(true);
@@ -78,4 +76,60 @@ test('updateTaskChecked updates the completion status of the task successfully',
     updateTaskChecked(daysKey, 0, false);
     const dayData = getDaysData(daysKey);
     expect(dayData.tasks[0].completed).toBe(false);
+});
+
+test('addCustomTagStorage adds one tag successfully', () => {
+    addCustomTagStorage('testTag1');
+    const tagData = getCustomTags();
+    expect(tagData.testTag1).toBe('red');
+});
+
+test('addCustomTagStorage adds multiple tags successfully', () => {
+    addCustomTagStorage('testTag2');
+    let tagData = getCustomTags();
+    expect(tagData.testTag2).toBe('blue');
+
+    addCustomTagStorage('testTag3');
+    tagData = getCustomTags();
+    expect(tagData.testTag3).toBe('pink');
+});
+
+test('addCustomTagStorage loops around to the beginning of the color list', () => {
+    // these 3 tags are added so we have enough tags to loop around
+    addCustomTagStorage('testTag4');
+    let tagData = getCustomTags();
+    expect(tagData.testTag4).toBe('green');
+
+    addCustomTagStorage('testTag5');
+    tagData = getCustomTags();
+    expect(tagData.testTag5).toBe('violet');
+
+    addCustomTagStorage('testTag6');
+    tagData = getCustomTags();
+    expect(tagData.testTag6).toBe('orange');
+
+    // this is the loop around case
+    addCustomTagStorage('testTag7');
+    tagData = getCustomTags();
+    expect(tagData.testTag7).toBe('red');
+});
+
+test('addCustomTagStorage does not change the color of an existing tag', () => {
+    // testTag6 previously was defined and given an orange color above (that's the expected behavior).
+    // re-adding it should NOT change that
+    addCustomTagStorage('testTag6');
+    const tagData = getCustomTags();
+    expect(tagData.testTag6).toBe('orange');
+});
+
+test('getCustomTagColor returns proper colors for tags that exist', () => {
+    let color = getCustomTagColor('testTag6');
+    expect(color).toBe('orange');
+
+    color = getCustomTagColor('testTag1');
+    expect(color).toBe('red');
+});
+
+test('getCustomTagColor for tag that does not exist should return empty string - after some tags are added', () => {
+    expect(getCustomTagColor('nonexistentTag2')).toBe('');
 });
